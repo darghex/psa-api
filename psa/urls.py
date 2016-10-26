6b3f87ac9b0617c30d3f8c4a853fbc4876b219e9
darghex <darghex@gmail.com>
2016-10-25 21:45:31 -0500

    include readme

--- psa/urls.py
+++ psa/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'psa.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^login/', login, name='login'),
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
